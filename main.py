4f8315e5703d7502a6a2afa50b2c8f228b0cde37
Samuel Witte <witte.sam@gmail.com>
2017-03-20 19:14:31 -0700

    change q_obs goal

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ from scipy.stats import poisson
 
 path = os.getcwd()
 
+QUIET = False
+
 model = "sigma_si"  # [sigma_si, sigma_sd, sigma_anapole, sigma_magdip, sigma_elecdip,
                     # sigma_LS, sigma_f1, sigma_f2, sigma_f3, sigma_si_massless,
                     # sigma_sd_massless, sigma_anapole_massless, sigma_magdip_massless,
@@ -27,7 +29,7 @@ model = "sigma_si"  # [sigma_si, sigma_sd, sigma_anapole, sigma_magdip, sigma_el
 
 coupling = "fnfp" + model[5:]
 mass = 6.
-sigmap = 7.*10.**-45.
+sigmap = 3.*10.**-45.
 fnfp = 1.
 delta = 0.  # Only works for elastic scattering at this point
 
@@ -59,15 +61,15 @@ drdq_params[coupling] = fnfp
 drdq_params['GF'] = GF
 drdq_params['time_info'] = time_info
 
-# 3\sigma for Chi-square Dist with 1 DoF means q = 8.85
-q_goal = 8.85
+# 3\sigma for Chi-square Dist with 1 DoF means q = 9.0
+q_goal = 9.0
 
 
 # make sure there are enough points for numerical accuracy/stability
 er_list = np.logspace(np.log10(Qmin), np.log10(Qmax), 500)
 time_list = np.zeros_like(er_list)
 
-# DM spectrum -- Careful, this needs to be implimented in a better way!
+
 dm_spec = dRdQ(er_list, time_list, **drdq_params) * 10. ** 3. * s_to_yr
 dm_rate = R(Qmin=Qmin, Qmax=Qmax, **drdq_params) * 10. ** 3. * s_to_yr * exposure
 dm_pdf = dm_spec / dm_rate
@@ -75,11 +77,8 @@ cdf_dm = dm_pdf.cumsum()
 cdf_dm /= cdf_dm.max()
 dm_events_sim = int(dm_rate * exposure)
 
-#np.savetxt(file_info + '_DM.dat', np.column_stack((er_list, dm_spec)))
-print 'Predicted Number of DM events: {}'.format(dm_events_sim)
-nevts_dm = poisson.rvs(int(dm_events_sim))
 
-# TODO generalize beyond B8 + DM
+# TODO generalize beyond B8
 nu_comp = 'B8'
 # neutrino ER spectrum
 nuspec = np.zeros_like(er_list)
@@ -91,52 +90,69 @@ nu_pdf = nuspec / nu_rate
 cdf_nu = nu_pdf.cumsum()
 cdf_nu /= cdf_nu.max()
 Nu_events_sim = int(nu_rate * exposure)
-#np.savetxt(file_info + '_nub8.dat', np.column_stack((er_list, nuspec)))
-
-print 'Predicted Number of Nu events: {}'.format(Nu_events_sim)
-nevts_n = poisson.rvs(int(Nu_events_sim))
 
 
-# Simulate events
-Nevents = nevts_n + nevts_dm
-print 'Simulation {:.0f} events...'.format(Nevents)
-u = random.rand(Nevents)
-# Generalize to rejection sampling algo for time implimentation
-e_sim = np.zeros(Nevents)
-for i in range(Nevents):
-    if i < int(nevts_n):
-        e_sim[i] = er_list[np.absolute(cdf_nu - u[i]).argmin()]
-    else:
-        e_sim[i] = er_list[np.absolute(cdf_dm - u[i]).argmin()]
-times = np.zeros_like(e_sim)
-#print e_sim
+n_runs = 50
+tstat_arr = np.zeros(n_runs)
+# While loop goes here. Fill tstat_arr for new sims and extract median/mean
+nn = 0
+while nn < n_runs:
+    print 'Run {:.0f} of {:.0f}'.format(nn, n_runs)
+    nevts_dm = poisson.rvs(int(dm_events_sim))
+    nevts_n = poisson.rvs(int(Nu_events_sim))
+    if not QUIET:
+        print 'Predicted Number of Nu events: {}'.format(Nu_events_sim)
+        print 'Predicted Number of DM events: {}'.format(dm_events_sim)
 
-print 'Running Likelihood Analysis...'
-# Minimize likelihood -- MAKE SURE THIS MINIMIZATION DOESNT FAIL. CONSIDER USING GRADIENT INFO
-like_init_nodm = Likelihood_analysis(model, coupling, mass, 0., fnfp,
-                                     exposure, element, experiment_info, e_sim, times,
-                                     Qmin=Qmin, Qmax=Qmax, time_info=time_info, GF=False)
-max_nodm = minimize(like_init_nodm.likelihood, np.array([0.]), args=(np.array([-100.])), tol=0.01)
-#print max_nodm
+    # Simulate events
+    Nevents = nevts_n + nevts_dm
+    if not QUIET:
+        print 'Simulation {:.0f} events...'.format(Nevents)
+    u = random.rand(Nevents)
+    # Generalize to rejection sampling algo for time implimentation
+    e_sim = np.zeros(Nevents)
+    for i in range(Nevents):
+        if i < int(nevts_n):
+            e_sim[i] = er_list[np.absolute(cdf_nu - u[i]).argmin()]
+        else:
+            e_sim[i] = er_list[np.absolute(cdf_dm - u[i]).argmin()]
+    times = np.zeros_like(e_sim)
+    #print e_sim
 
-like_init_dm = Likelihood_analysis(model, coupling, mass, 1., fnfp,
-                                   exposure, element, experiment_info, e_sim, times,
-                                   Qmin=Qmin, Qmax=Qmax, time_info=time_info, GF=False)
-max_dm = minimize(like_init_dm.like_multi_wrapper, np.array([0., np.log10(sigmap)]), tol=0.01,
-                jac=False)
+    if not QUIET:
+        print 'Running Likelihood Analysis...'
+    # Minimize likelihood -- MAKE SURE THIS MINIMIZATION DOESNT FAIL. CONSIDER USING GRADIENT INFO
+    like_init_nodm = Likelihood_analysis(model, coupling, mass, 0., fnfp,
+                                         exposure, element, experiment_info, e_sim, times,
+                                         Qmin=Qmin, Qmax=Qmax, time_info=time_info, GF=False)
+    max_nodm = minimize(like_init_nodm.likelihood, np.array([0.]), args=(np.array([-100.])), tol=0.01)
+    #print max_nodm
 
+    like_init_dm = Likelihood_analysis(model, coupling, mass, 1., fnfp,
+                                       exposure, element, experiment_info, e_sim, times,
+                                       Qmin=Qmin, Qmax=Qmax, time_info=time_info, GF=False)
+    max_dm = minimize(like_init_dm.like_multi_wrapper, np.array([0., np.log10(sigmap)]), tol=0.01,
+                      jac=False)
 
-print 'BF Neutrino normalization without DM: {:.2e}'.format(10.**max_nodm.x[0])
-print 'BF Neutrino normalization with DM: {:.2e}'.format(10.**max_dm.x[0])
-print 'BF DM sigma_p: {:.2e} \n\n'.format(10.**max_dm.x[1])
+    if not QUIET:
+        print 'BF Neutrino normalization without DM: {:.2e}'.format(10.**max_nodm.x[0])
+        print 'BF Neutrino normalization with DM: {:.2e}'.format(10.**max_dm.x[0])
+        print 'BF DM sigma_p: {:.2e} \n\n'.format(10.**max_dm.x[1])
 
-test_stat = np.max([max_nodm.fun - max_dm.fun, 0.])
+    test_stat = np.max([max_nodm.fun - max_dm.fun, 0.])
 
-pval = chi2.sf(test_stat,1)
+    pval = chi2.sf(test_stat,1)
 
+    if not QUIET:
+        print 'TS: ', test_stat
+        print 'p-value: ', pval
+
+    tstat_arr[nn] = test_stat
+    nn += 1.
+
+print 'FINISHED CYCLE \n'
 print 'True DM mass: ', mass
 print 'True DM sigma_p: ', sigmap
-print 'TS: ', test_stat
-print 'p-value: ', pval
-
+print 'Median Q: {:.2f}'.format(np.median(tstat_arr))
+print 'Mean Q: {:.2f}'.format(np.mean(tstat_arr))
 
